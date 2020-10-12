be58a6bcdb4653a4f4bb5fc687c2f4fc79c95f6f
Anton Evmenenko <anton.evmenenko.93@gmail.com>
2020-10-12 15:30:33 +0200

    scroll to top button updated

--- assets/js/scrollup.js
+++ assets/js/scrollup.js
@@ -1,20 +1,55 @@
 $(document).ready(function () {
-    var mobile = screen.width < 500 ||
+    let mobile = screen.width < 500 ||
         navigator.userAgent.match(/Android/i) ||
         navigator.userAgent.match(/webOS/i) ||
         navigator.userAgent.match(/iPhone/i) ||
         navigator.userAgent.match(/iPod/i);
+    let previousPosition = 0;
+    let autoScroll = false;
+    let rotateScrollupIcon = function (rotate) {
+        $('.icon-scrollup').css('transform', 'translate(-50%, -50%) ' + (rotate ? 'rotate(180deg)' : ''));
+    };
 
     $(window).scroll(function () {
-        if (!mobile) {
-            if ($(this).scrollTop() > 100) {
-                $('.scrollup').show();
-                $('.icon-scrollup').fadeIn();
-            } else {
-                $('.icon-scrollup').fadeOut(function(){
+        if (autoScroll || mobile) {
+            return;
+        }
+
+        var position = $(document).scrollTop();
+
+        if (position != 0 && position != previousPosition) {
+            previousPosition = 0;
+            rotateScrollupIcon(false);
+        }
+
+        if ($(this).scrollTop() > 100) {
+            $('.scrollup').show();
+            $('.icon-scrollup').fadeIn();
+        } else {
+            if (previousPosition == 0) {
+                $('.icon-scrollup').fadeOut(function () {
                     $('.scrollup').hide();
                 });
             }
         }
+    });
+
+    $('.scrollup').click(function () {
+        if (mobile) {
+            return;
+        }
+
+        let currentPosition = $(document).scrollTop();
+        let goalPosition = (currentPosition == 0 && previousPosition != 0) ? previousPosition : 0;
+
+        previousPosition = currentPosition;
+        rotateScrollupIcon(goalPosition == 0);
+        autoScroll = true;
+        $("html, body").animate({
+            scrollTop: goalPosition
+        }, 600, function () {
+            autoScroll = false;
+        });
+        return false;
     });
 });
