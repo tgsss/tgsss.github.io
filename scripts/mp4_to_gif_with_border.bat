353478f31473907e873da425f6e79f0c7f2660d2
Anton Evmenenko <anton.evmenenko.93@gmail.com>
2023-10-20 01:03:29 +0200

    mp4_to_gif_with_border.bat updated

--- scripts/mp4_to_gif_with_border.bat
+++ scripts/mp4_to_gif_with_border.bat
@@ -3,12 +3,12 @@ setlocal
 set input_file=input.mp4
 set output_file=output.gif
 
-set crop=2 & :: pixels
+set crop=4 & :: pixels
 set border_thickness=2 & :: pixels
 set skip=1 & :: seconds
 
 set /a temp=%border_thickness% * 2
 
-ffmpeg -i %input_file% -ss %skip% -filter_complex "[0]crop=w=iw-%crop%:h=ih-%crop%[a];[a]pad=w=%temp%+iw:h=%temp%+ih:x=%border_thickness%:y=%border_thickness%:color=black" %output_file%
+ffmpeg -i %input_file% -ss %skip% -filter_complex "[0]crop=w=iw-%crop%:h=ih-%crop%[a];[a]pad=w=%temp%+iw:h=%temp%+ih:x=%border_thickness%:y=%border_thickness%:color=gray" %output_file%
 
 endlocal
